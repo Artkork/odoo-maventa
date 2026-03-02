37d4a2f8577b620a2768b422a9389b673cb42d9e
Artkork <arto.korkeamaki@taivaspaikka.fi>
2026-03-02 02:09:16 +0200

    Add odoo.sh configuration and requirements.txt for automated deployment

---
+++ .odoo.sh
@@ -0,0 +1,11 @@
+{
+  "manifest": "maventa_finvoice/__manifest__.py",
+  "server_wide_modules": "",
+  "addons_path": ".",
+  "external_dependencies": {
+    "python": [
+      "requests",
+      "lxml"
+    ]
+  }
+}
